f6377f97e00a96e97b3238fcc8c89d5ee4412d07
MarkSelden <seldenm@berkeley.edu>
2020-01-13 23:34:05 -0800

    Updates

---
+++ precache-manifest.4858320105fc4c53699a142ce55bcdc0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a42d5945d1b7fb8d6444541cdcf3ed",
+    "url": "/mark-selden/index.html"
+  },
+  {
+    "revision": "9642d3b0d901bde7d7da",
+    "url": "/mark-selden/static/css/main.51fb2156.chunk.css"
+  },
+  {
+    "revision": "6eaaa5c8c1835b672fa5",
+    "url": "/mark-selden/static/js/2.2803d68b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mark-selden/static/js/2.2803d68b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9642d3b0d901bde7d7da",
+    "url": "/mark-selden/static/js/main.ff1f3a01.chunk.js"
+  },
+  {
+    "revision": "c6b9ba378141a8a66dfa",
+    "url": "/mark-selden/static/js/runtime-main.1eedfb9f.js"
+  },
+  {
+    "revision": "4e059d93412aad5a5bc86fec691b0d9e",
+    "url": "/mark-selden/static/media/Me.4e059d93.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mark-selden/static/media/logo.5d5d9eef.svg"
+  }
+]);
